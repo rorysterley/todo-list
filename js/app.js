63a56af694d2bf47667fa98c771831954abe9ebc
Rory Sterley <rorysterley@gmail.com>
2015-01-01 14:06:17 -0800

    Add comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
- // Add user input to the list when the "enter/return" key is pressed.
 $('document').ready(function() { 
   $('#userinput').focus();
 
+  // Add user input to the list when the "enter/return" key is pressed.
   $('#userinput').keyup(function(e) {
     if(e.keyCode == 13) { 
       var input = "<li>" + $('#userinput').val() + "</li>"; 
@@ -10,6 +10,7 @@ $('document').ready(function() {
     }
   });
 
+  // Remove list item on click
   $('#todolist').on('click', 'li', function(event) {
     $(this).remove();
   });
